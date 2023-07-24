f9defc695c6ba1ec8ad3518285278cb61bb09b95
Yacol87 <bukala.jacek@gmail.com>
2023-07-24 17:07:36 +0200

    Full push

--- Tests.py
+++ Tests.py
@@ -1,7 +1,8 @@
-courses = ["c", "c++", "python", "java", "pandas"]
-courses1 = ["java","python","pandas","c"]
-for i in courses:
-       if i in courses1:
-           print(i)
-       else:
-           print("noono")
+# courses = ["c", "c++", "python", "java", "pandas"]
+# courses1 = ["java","python","pandas","c"]
+# for i in courses:
+#        if i in courses1:
+#            print(i)
+#        else:
+#            print("noono")
+#TEST TEST
