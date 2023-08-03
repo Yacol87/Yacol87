f9fe1581edf2c9eaa007cbfb82683b1b7183d869
Yacol87 <bukala.jacek@gmail.com>
2023-08-03 10:16:09 +0200

    Dev 1 branch commit

---
+++ BaldNotes.py
@@ -0,0 +1,15 @@
+SETUP
+# Settings - Ctrl + Alt + 0
+# Git
+#
+#
+#
+#
+#
+#
+#
+CODING
+# Comment all - Ctrl + /
+# Shift Shift - search everywhere
+# F2 - jump between issues
+# Ctrl + W - extend selection
