4b897048708d885ac8ca23a967db245993556def
Yacol87 <bukala.jacek@gmail.com>
2023-08-03 12:46:45 +0200

    Dev 1 branch commit

---
+++ Class Kids.py
@@ -0,0 +1,8 @@
+class Kid:
+
+    def ask(self):
+        print("Tataaa, a co toooo?")
+
+ala = Kid()
+
+print(type(ala))
--- Dict list.py
+++ Dict list.py
@@ -1,12 +1,34 @@
-my_dict = { "1": "One",
-            "2": "Two",
-            "7": "Seven"
-            }
+# DICTIONARY
+# my_dict = { "1": "One",
+#             "2": "Two",
+#             "7": "Seven"
+#             }
+#
+# print (my_dict.keys(), "My dict keys")
+# print (my_dict.values(), "My dict values")
+# print ("get my value for key  " + my_dict.get("7"))
+# print (my_dict.pop("2"))
+# print (my_dict)
+# my_dict.pop("1",1)
+# print (my_dict)
 
-print (my_dict.keys(), "My dict keys")
-print (my_dict.values(), "My dict values")
-print ("get my value for key  " + my_dict.get("7"))
-print (my_dict.pop("2"))
-print (my_dict)
-my_dict.pop("1",1)
-print (my_dict)
+
+
+
+#LISTS
+fruit_list = ["apple", "banana", "cherry","plum"]
+number_list = [1, 5, 7, 9, 11, 13]
+boolean_list = [True, False, False]
+
+print(fruit_list, number_list, boolean_list)
+
+x1 = 11
+x2= 0
+while x1 < 20:
+    print(number_list[x2], fruit_list[x2])
+    x1 += 3
+    x2 +=1
+    # print(x1)
+
+# for x in fruit_list:
+#     print(x)
