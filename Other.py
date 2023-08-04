63d504180a9f465ed1dafe5b0d73482347247267
Yacol87 <bukala.jacek@gmail.com>
2023-08-04 12:38:00 +0200

    Dev 1 branch commit

--- Other.py
+++ Other.py
@@ -7,19 +7,20 @@ diki = { "Jen" : "Wesoly moczymorda i leń, można pogadać i połazić",
 def give_relation():
 
     print ("Following people are in the room: " + str(list(diki.keys())))
-    check_relation = input ("With whom would you like to check relation?")
+    check_relation = input ("With whom would you like to check relation? ")
     print ("Who are you " + check_relation)
     print ("I am your  " + diki[check_relation])
-    wanna_check2 = input("do you want to check more relations? Say Yes or No")
+    wanna_check2 = input("do you want to check more relations? Say Yes or No ")
     if wanna_check2 in ("Yes","yes"):
         give_relation()
     else:
         print("no more checking")
 
 
-wanna_check_relation = input("Want to check relation with someone? Yes or No")
+wanna_check_relation = input("Want to check relation with someone? Yes or No ")
 
 
 if wanna_check_relation in ("Yes","yes","y"):
      give_relation()
+else: print("I guess no checking today ")
 
